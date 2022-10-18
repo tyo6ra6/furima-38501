3a658fcafa20cd2b84e5b6fe00fc2b9c1a390a51
tyo6ra6 <maako527@icloud.com>
2022-10-18 12:13:50 +0900

    ユーザー管理機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,12 @@
 ActiveRecord::Schema.define(version: 2022_10_17_093541) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "first_name_kana", null: false
+    t.string "last_name_kana", null: false
+    t.date "bithday", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
