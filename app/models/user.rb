5a152f3d9ab29e8652d91aa1a9541d602c01d7f0
tyo6ra6 <maako527@icloud.com>
2022-10-18 15:06:57 +0900

    正規表現の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,11 @@ class User < ApplicationRecord
          validates :last_name_kana, presence: true
          validates :bithday, presence: true
          
-       
+         
+
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+
        
 
 end
