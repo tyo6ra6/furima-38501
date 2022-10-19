c8f4896d426082a045ec36c5fbb5d59140b5d98d
tyo6ra6 <maako527@icloud.com>
2022-10-19 17:33:34 +0900

    レビュー修正3

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname {Faker::Name.name}
     email {Faker::Internet.free_email}
-    password {Faker::Internet.password(min_length: 6)}
+    password {"a1" + Faker::Internet.password(min_length: 4)}
     password_confirmation {password}
     first_name        {'中川'}   
     last_name         {'涼太'}  
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -45,15 +45,15 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Email is invalid") 
       end
      
-      it 'passwordが6文字以下では登録できない' do
-        @user.password = '12345'
-        @user.password_confirmation = '12345'
+      it 'passwordが5文字以下では登録できない' do
+        @user.password = '1a234'
+        @user.password_confirmation = '1a234'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
       end
-      it 'passwordが半角英数字混合でなければ登録できない' do
-        @user.password = 'aaaaaa'
-        @user.password_confirmation = 'aaaaaa'
+      it 'パスワードに全角文字を含むと登録できない' do
+        @user.password = 'a1aaaあ'
+        @user.password_confirmation = 'a1aaaあ'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
       end
