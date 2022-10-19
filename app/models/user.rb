1dc3da970fafeda3d58f9a1dd1e287777440f5ee
tyo6ra6 <maako527@icloud.com>
2022-10-19 17:55:06 +0900

    レビュの修正４

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
 
          validates :last_name, presence: true,
                    format: { 
-                    with: /\A[ぁ-んァ-ン一-龥]/,
+                    with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,
                     message: "全角文字を使用してください"
                     } 
 
